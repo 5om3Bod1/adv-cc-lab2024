dde7f5f0dd7d1124a96bc4837c919e0ece800a5f
5om3Bod1 <143824420+5om3Bod1@users.noreply.github.com>
2024-04-11 01:15:49 -0400

    Delete ccProject3 directory

--- ccProject3/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-
-    let questions = document.querySelectorAll('.question');
-    for (var i = questions.length - 1; i >= 0; i--){
-        radios[i].addEventListener('change', function(){
-            localStorage.setItem("result", this.value)
-        })
-    }
-})
